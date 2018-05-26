c5ad73260c1ab262619e69005c9d8ab58c80c78b
Amit Gupta <amitgupta.gwl@gmail.com>
2018-05-26 12:56:09 +0530

    remove extra tag

--- js/elements.js
+++ js/elements.js
@@ -18,63 +18,6 @@ riot.tag2('r-path', '<path riot-d="{getPath()}" fill="rgba(100,200,200,0.2)" str
                                 z`
         }
 });
-riot.tag2('rold-rect', '<rect ref="rect" onmousedown="{hold}"></rect>', '', '', function(opts) {
-            tag = this;
-            tag.diffX = 0;
-            tag.diffY = 0;
-            tag.dragStart = false;
-            tag.hold = hold;
-            tag.drag = drag;
-            tag.release = release;
-            tag.x= Number.parseInt(opts.x);
-            tag.y= Number.parseInt(opts.y);
-            tag.updatePosition = updatePosition;
-            tag.setStartPosition = setStartPosition;
-
-            tag.on("mount", function(e) {
-                console.log(tag)
-                tag.refs.rect.setAttribute("x", opts.x);
-                tag.refs.rect.setAttribute("y", opts.y);
-                tag.refs.rect.setAttribute("width", opts.width);
-                tag.refs.rect.setAttribute("height", opts.height);
-                opts.rx && (tag.refs.rect.setAttribute("rx", opts.rx) );
-                opts.ry && (tag.refs.rect.setAttribute("ry", opts.ry) );
-            })
-
-            function updatePosition(cursor){
-
-                var x = (cursor.x - tag.diffX);
-                var y = (cursor.y - tag.diffY);
-
-                cursor.target.setAttribute("x", x);
-                cursor.target.setAttribute("y", y);
-
-            }
-
-            function setStartPosition(cursor){
-                tag.diffX = cursor.x - tag.x;
-                tag.diffY = cursor.y - tag.y;
-            }
-
-            function hold(e){
-                if(opts.draggable === "true"){
-                    tag.setStartPosition(e);
-                    e.target.addEventListener("mousemove", drag);
-                    e.target.addEventListener("mouseup", release);
-                }
-            }
-
-            function drag(e){
-                tag.updatePosition(e);
-            }
-
-            function release(e){
-                e.target.removeEventListener("mousemove", drag);
-                e.target.removeEventListener("mouseup", release);;
-            }
-});
-riot.tag2('r-svg', '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"> <yield></yield> </svg>', '', '', function(opts) {
-});
 riot.tag2('r-rect', '<rect ref="rect" onmousedown="{hold}" class="{draggable: draggable}"></rect> <g if="{resizable}" class="resize-handlers"> <circle class="resize-handler nw" ref="nw"></circle> <circle class="resize-handler n" ref="n"></circle> <circle class="resize-handler ne" ref="ne"></circle> <circle class="resize-handler w" ref="w"></circle> <circle class="resize-handler e" ref="e"></circle> <circle class="resize-handler sw" ref="sw"></circle> <circle class="resize-handler s" ref="s"></circle> <circle class="resize-handler se" ref="se"></circle> </g>', 'r-rect .draggable,[data-is="r-rect"] .draggable{ cursor: move; } r-rect .resize-handler,[data-is="r-rect"] .resize-handler{ fill: lavender; fill-opacity: 0.5; stroke: gray; stroke-width: 1px; vector-effect: non-scaling-stroke; r: 5 } r-rect .ne,[data-is="r-rect"] .ne{ cursor: nesw-resize} r-rect .n,[data-is="r-rect"] .n{ cursor: row-resize} r-rect .nw,[data-is="r-rect"] .nw{ cursor: nwse-resize} r-rect .e,[data-is="r-rect"] .e{ cursor: col-resize} r-rect .w,[data-is="r-rect"] .w{ cursor: col-resize} r-rect .se,[data-is="r-rect"] .se{ cursor: nwse-resize} r-rect .s,[data-is="r-rect"] .s{ cursor: row-resize} r-rect .sw,[data-is="r-rect"] .sw{ cursor: nesw-resize}', '', function(opts) {
         var tag = this;
         tag.diffX = 0;
@@ -130,7 +73,6 @@ riot.tag2('r-rect', '<rect ref="rect" onmousedown="{hold}" class="{draggable: dr
 
                 tag.refs.sw.setAttribute("cx", tag.x);
                 tag.refs.sw.setAttribute("cy", tag.y + tag.h);
-
             }
         }
 
@@ -168,4 +110,6 @@ riot.tag2('r-rect', '<rect ref="rect" onmousedown="{hold}" class="{draggable: dr
             e.target.removeEventListener("mouseup", release);;
         }
 
+});
+riot.tag2('r-svg', '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"> <yield></yield> </svg>', '', '', function(opts) {
 });
